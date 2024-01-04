4d05f7c4a995fb6f5ed98c3d041e795e8be9d5e7
tgaibotz <152408509+tgaibotz@users.noreply.github.com>
2024-01-04 12:33:17 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class Bot(Client):
         me = await self.get_me()
         self.username = '@' + me.username
             
-        print('Bot Started Powered By @VJ_BOTZ)
+        print('Bot Started Powered By @THE_AI_BOTZ')
 
 
     async def stop(self, *args):
