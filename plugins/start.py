e2c0511f6a9318abcb4c14838fa262195a738fdd
tgaibotz <152408509+tgaibotz@users.noreply.github.com>
2024-01-04 12:40:45 +0500

    start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,16 +21,16 @@ async def start_message(c,m):
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
-        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[Tᴇᴄʜ VJ](https://t.me/vj_botz)**",
+    await m.reply_photo(f"https://telegra.ph/AI-11-01-4",
+        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[AI BOTZ](https://t.me/THE_AI_BOTZ)**",
         reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+                        InlineKeyboardButton('🤖 AI BOTZ MENU 🤖', url='https://t.me/AI_BOTZ_MENU_BOT')
                     ],  
                     [
-                        InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'),
-                        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz')
+                        InlineKeyboardButton("🔺MAIN CGANNEL🔺️", url='https://t.me/THE_AI_BOTZ'),
+                        InlineKeyboardButton("❓SUPPORT GROUP ❓", url='https://t.me/AI_BOTZ_SUPPORT')
                     ]
                 ]
             )
