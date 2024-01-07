f2311cd88ee140d94903db53727b3fa47e875d3a
tgaibotz <152408509+tgaibotz@users.noreply.github.com>
2024-01-07 17:53:42 +0500

    Update codes

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-        "vj chatgpt bot",
+        "AI BOTZ CHATGPT BOT",
          api_id=API_ID,
          api_hash=API_HASH,
          bot_token=BOT_TOKEN,
