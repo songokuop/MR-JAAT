fbdf8ffd3bfda4459c492101693ec14a83e5564c
AI REPOS 23 <93036340+ai-botz@users.noreply.github.com>
2024-01-13 17:02:59 +0500

    FIx broadcast not working properly  bu @the_ai_botz

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -37,7 +37,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='⭕ Broadcasting your messages...'
+        text='reply_text'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
