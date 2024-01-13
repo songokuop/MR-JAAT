0acc103967e63ff5fdeb85318140f06786a20b4e
songokuop <149889315+songokuop@users.noreply.github.com>
2024-01-13 19:20:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -22,15 +22,15 @@ async def start_message(c,m):
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
     await m.reply_photo(f"https://telegra.ph/AI-11-01-4",
-        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ⁴ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[Movies_Zone_Media](https://t.me/{AIS})**",
+        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ⁴ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[Movies_Zone_Media](https://t.me/Movies_Zone_Media)**",
         reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('🤖 Movies Zone MENU 🤖', url='https://t.me/{MENUTXT}')
+                        InlineKeyboardButton('🤖 Movies Zone MENU 🤖', url='https://t.me/Movies_Zone_Contact_Bot')
                     ],  
                     [
-                        InlineKeyboardButton("MAIN CHANNEL", url='https://t.me/{AIS}'),
-                        InlineKeyboardButton("SUPPORT GROUP", url='https://t.me/{SUPPORT}')
+                        InlineKeyboardButton("MAIN CHANNEL", url='https://t.me/Movies_Zone_Media'),
+                        InlineKeyboardButton("SUPPORT GROUP", url='https://t.me/Movies_Zone_Support')
                     ]
                 ]
             )
